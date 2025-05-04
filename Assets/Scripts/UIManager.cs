e58edda27133011e8935c459347583b7c245ee7a
Svemilio <emiliosava65@gmail.com>
2025-05-04 20:21:33 +0200

    add canvas functionality

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,11 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     private const int extraHeight = 930;
+    private string currentWorkoutName = "";
+    private bool addDayWorkout = false;
 
     [SerializeField]
     RectTransform contentExercises;
+    [SerializeField]
+    TextMeshProUGUI workoutName;
+    [SerializeField]
+    GameObject buttonAddDayWorkout;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +20,36 @@ public class UIManager : MonoBehaviour
     }
     public void AddExerciseToScrollView(Transform inputField)
     {
+        if(!addDayWorkout)
+        {
+            addDayWorkout = true;
+            ActiveButtonAddWorkout();
+        }
         Instantiate(inputField, contentExercises.transform);
         Vector2 currentSize = contentExercises.sizeDelta;
         currentSize.y += extraHeight;
         contentExercises.sizeDelta = currentSize;
+    }
+
+    public void UpdateWorkoutName(TextMeshProUGUI workoutName)
+    {
+        this.currentWorkoutName = workoutName.text;
+        this.workoutName.text = currentWorkoutName;
+    }
+
+    private void ActiveButtonAddWorkout()
+    {
+        buttonAddDayWorkout.SetActive(true);
+    }
+
+    public void ResetWorkoutCanvas()
+    {
+        addDayWorkout = false;
+        foreach (Transform child in contentExercises.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        workoutName.text = "";
+        buttonAddDayWorkout.SetActive(false);
     }
 }
